02e596608f59ae64f28fc3e7caf6ef1e61b1ff0d
codetill5 <mehrashubham216@gmail.com>
2022-07-07 16:25:21 +0530

    nft contract & deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,19 @@
 /**
- * @type import('hardhat/config').HardhatUserConfig
- */
+* @type import('hardhat/config').HardhatUserConfig
+*/
+require("@nomiclabs/hardhat-ethers");
+require('dotenv').config();
+
+const { API_URL, PRIVATE_KEY } = process.env;
+
 module.exports = {
-  solidity: "0.7.3",
-};
+   solidity: "0.8.2",
+   defaultNetwork: "rinkeby",
+   networks: {
+      hardhat: {},
+      rinkeby: {
+         url: API_URL,
+         accounts: [`0x${PRIVATE_KEY}`]
+      }
+   },
+}
